c2bf0db9e56cc71da1e2ae66a461665170b7654f
S KARTHIK <skarthik.link@gmail.com>
2026-03-11 10:22:14 +0530

    feat(uc2): retrieve employee payroll data using JDBC

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.employeepayroll;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class EmployeePayrollService {
+
+    public static void main(String[] args) {
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "astronaut";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection =
+                    DriverManager.getConnection(jdbcURL, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet =
+                    statement.executeQuery("select * from employee_payroll");
+
+            while(resultSet.next()) {
+
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+
+                System.out.println(id + " " + name + " " + salary);
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
