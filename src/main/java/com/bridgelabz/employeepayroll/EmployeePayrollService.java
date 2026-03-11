3e2cc7d706c1fcb8b089a577c4b5f9a11a8d54cd
S KARTHIK <skarthik.link@gmail.com>
2026-03-11 10:59:51 +0530

    feat(uc10): add employee with start date

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -2,9 +2,7 @@ package com.bridgelabz.employeepayroll;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class EmployeePayrollService {
 
@@ -17,37 +15,24 @@ public class EmployeePayrollService {
         try {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver Loaded");
 
             Connection connection =
                     DriverManager.getConnection(jdbcURL, username, password);
 
-            System.out.println("Connection Established Successfully");
-
-            Statement statement = connection.createStatement();
-
             String query =
-                    "select gender, sum(salary), avg(salary), min(salary), max(salary), count(*) from employee_payroll group by gender";
+                    "insert into employee_payroll (name, salary, gender, start) values (?, ?, ?, ?)";
 
-            ResultSet resultSet = statement.executeQuery(query);
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(query);
 
-            while(resultSet.next()) {
+            preparedStatement.setString(1, "Mark");
+            preparedStatement.setDouble(2, 450000);
+            preparedStatement.setString(3, "M");
+            preparedStatement.setString(4, "2024-01-10");
 
-                String gender = resultSet.getString(1);
-                double sum = resultSet.getDouble(2);
-                double avg = resultSet.getDouble(3);
-                double min = resultSet.getDouble(4);
-                double max = resultSet.getDouble(5);
-                int count = resultSet.getInt(6);
+            int rows = preparedStatement.executeUpdate();
 
-                System.out.println(
-                        gender + " | SUM=" + sum +
-                                " | AVG=" + avg +
-                                " | MIN=" + min +
-                                " | MAX=" + max +
-                                " | COUNT=" + count
-                );
-            }
+            System.out.println("Employee Inserted: " + rows);
 
         } catch (Exception e) {
             e.printStackTrace();
